c311054f8d8ad4dc27e671e119aaa5564ea0509f
hilmiwismadi <dzakiwismadi@gmail.com>
2024-12-27 13:59:28 +0700

    fix: tailiwind.config.ts dobule

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,6 @@ import type { Config } from "tailwindcss";
 
 export default {
     darkMode: ["class"],
-      darkMode: ["class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -70,7 +69,7 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")
+  plugins: [require("tailwindcss-animate"),
 	require("tailwindcss-animate"),
 	require("tailwind-scrollbar"),
   	require("tailwind-scrollbar-hide"),
