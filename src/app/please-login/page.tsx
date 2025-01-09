635b9d4472edc7370a75e2b5ec9af2573bc0e428
tsimiscouse <attaqi21@gmail.com>
2025-01-09 16:09:59 +0700

    adjust feedback for the task assignment modal in the task page

--- src/app/please-login/page.tsx
+++ src/app/please-login/page.tsx
@@ -1,12 +1,13 @@
+import Link from "next/link";
+
 export default function PleaseLoginPage() {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '20%' }}>
-        <h1>Please Login First</h1>
-        <p>You need to log in to access this page.</p>
-        <a href="/" style={{ textDecoration: 'underline', color: 'blue' }}>
-          Go to Login
-        </a>
-      </div>
-    );
-  }
-  
+  return (
+    <div style={{ textAlign: "center", marginTop: "20%" }}>
+      <h1>Please Login First</h1>
+      <p>You need to log in to access this page.</p>
+      <Link href="/" passHref>
+        <span style={{ textDecoration: "underline", color: "blue" }}>Go to Login</span>
+      </Link>
+    </div>
+  );
+}
